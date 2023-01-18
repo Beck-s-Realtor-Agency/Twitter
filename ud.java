050b55c1abd11609e83c7ec0fc1324ab3b07613c
fyfyjekwe <118690289+fyfyjekwe@users.noreply.github.com>
2023-01-18 21:06:06 +0100

    vi

--- ud.java
+++ ud.java
@@ -2,8 +2,11 @@
 I am Emmanuel
 This is team29 practice
 Ifeanyi wrote this
+<<<<<<< HEAD
  HEAD
  HEAD
 every users details must be placed here
  a6e277d (Update ud.java)
  a6e277d (Update ud.java)
+=======
+>>>>>>> a6e277d (Update ud.java)
