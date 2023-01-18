c170c4e2eb07fc5730fc97aadaa69e8b033eca7b
fyfyjekwe <118690289+fyfyjekwe@users.noreply.github.com>
2023-01-18 21:06:06 +0100

    gone

--- ud.java
+++ ud.java
@@ -2,7 +2,8 @@
 I am Emmanuel
 This is team29 practice
 Ifeanyi wrote this
-<<<<<<< HEAD
+ HEAD
+ HEAD
 every users details must be placed here
-=======
->>>>>>> a6e277d (Update ud.java)
+ a6e277d (Update ud.java)
+ a6e277d (Update ud.java)
